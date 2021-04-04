094522795f5cb334b6fc4861b14ccc04738bab42
LimaGustav <LimaGustav@users.noreply.github.com>
2021-04-03 21:49:52 -0300

    js 03-04

---
+++ JS/miniProjetos/encontreOString/script/scripts.js
@@ -0,0 +1,18 @@
+// Criar um metodo para ler propriedades de um objeto
+// Exibir somente as propriedades do tipo string deste objeto
+
+let mensagem = mostraString()
+console.log(mensagem)
+
+let filme = {
+    titulo :'vingadores',
+    ano : 2016,
+    diretor : 'não sei',
+    assisti : 'sim',
+}
+
+console.log()
+
+function mostraString(obj) {
+    
+}
