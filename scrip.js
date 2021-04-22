ebf262b730a8bd4fffddfde25924435b45be1325
LimaGustav <lima.gustavo100@homtail.com>
2021-04-22 01:07:06 -0300

    Responsividade concluida

--- scrip.js
+++ scrip.js
@@ -1,1 +1,27 @@
+const navSlide = () => {
+    const burguer = document.querySelector('.burguer')
+    const nav = document.querySelector('.nav_header')
+    const navLinks = document.querySelectorAll('.nav_header a')
 
+    //HAMBURGUER MENU
+    burguer.addEventListener("click", () => {
+        //ANIMAÇÃO HAMBURGUER
+        nav.classList.toggle("nav_ativa")
+
+        //ANIMAÇÃO LINKS
+        navLinks.forEach((link, index)=> {
+            if(link.style.animation){
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navHeaderFade 0.5s ease forwards ${index / 7 + 0.4}s`
+            }
+        })
+        // ANIMAÇÃO X DO BURGUER
+        burguer.classList.toggle('toggle')
+
+    })
+
+}
+
+navSlide()
